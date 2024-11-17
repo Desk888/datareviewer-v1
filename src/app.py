848e6a81a2827c893933ab0832f48795b86f2642
Desk888 <work.lorenzofilippinI@gmail.com>
2024-11-17 13:21:11 +0000

    changed remote, added more functionalities in the app.py and restructured main.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import json
 import pandas as pd
 from datetime import datetime
 from dotenv import load_dotenv
@@ -17,8 +16,9 @@ directory = os.getenv("DIRECTORY")
 def open_files():
     """
     Extracts data from all files in the data directory,
-    and processes them into a single DataFrame.
+    and processes them into a list of DataFrames.
     """
+    data_frames = []
     try:
         for filename in os.listdir(directory):
             if filename.endswith(('.csv', '.json', '.xlsx')):
@@ -31,99 +31,107 @@ def open_files():
 
                 if filename.endswith('.csv'):
                     df = pd.read_csv(filepath)
-                    
-                elif filename.endswith('.json'):
-                    try:
-                        with open(filepath, 'r') as file:
-                            data = json.load(file)
-                            if isinstance(data, dict):
-                                first_list = next((value for value in data.values() if isinstance(value, list)), None)
-                                
-                                if first_list is not None:
-                                    df = pd.json_normalize(first_list)
-                                else:
-                                    logger.error(f' [⛔️] No list found in JSON structure for file: {filename}')
-                                    continue
-                            else:
-                                logger.error(f' [⛔️] Unexpected JSON structure for file: {filename}')
-                                continue
-                            
-                    except json.JSONDecodeError as json_err:
-                        logger.error(f' [⛔️] Error decoding JSON from file: {filename}', exc_info=json_err)
-                        continue
-                    
-                elif filename.endswith('.xlsx'):
-                    df = pd.read_excel(filepath)
 
                 if df is not None:
                     df['fetched_at'] = date_time
                     logger.info(f' [✅] Opened File {filename} Successfully.')
-                    logger.info(f' [✅] DataFrame Created Successfully.')
-                    return df 
-                    
-
+                    data_frames.append(df)
+                else:
+                    logger.warning(f" [⚠️] File {filename} could not be processed.")
+        
+        if not data_frames:
+            logger.warning(" [⚠️] No valid files found to process.")
+        
     except Exception as e:
-        logger.error(f' [⛔️] Error Processing Files in the directory: {directory}', exc_info=True)
+        logger.error(f' [⛔️] Error processing files in the directory: {directory}', exc_info=True)
+    
+    return pd.concat(data_frames, ignore_index=True) if data_frames else pd.DataFrame()
 
 # ////////////////////////////////////////////////////////////////////////////////////
 
 def remove_duplicates(df, subset=None, keep='first', log_duplicates=True):
     """
     Finds and removes duplicate rows in the DataFrame.
-    Returns a new DataFrame with duplicates removed.
     """
-    # Find duplicate rows
-    duplicate_rows = df[df.duplicated(subset=subset, keep=False)]
     logger.info(" [..] Checking for duplicate rows in the DataFrame...")
+    duplicate_count = df.duplicated(subset=subset, keep=False).sum()
+    
     if log_duplicates:
-        if duplicate_rows.empty:
+        if duplicate_count == 0:
             logger.info(" [✅] No duplicate rows found.")
         else:
-            logger.warning(f" [{len(duplicate_rows)}] duplicate rows found. Removing duplicates.")
-            logger.debug(f"Duplicate rows:\n{duplicate_rows}")
+            logger.warning(f" [{duplicate_count}] duplicate rows found. Removing duplicates.")
     
     # Remove duplicates
     df_cleaned = df.drop_duplicates(subset=subset, keep=keep).reset_index(drop=True)
-    
     return df_cleaned
+
 
 # ////////////////////////////////////////////////////////////////////////////////////
 
 def check_missing_values(df_cleaned):
     """
-    Checks for missing values in the given DataFrame (File).
+    Checks for missing values in the given DataFrame.
     """
     logger.info(" [..] Checking for missing values in the DataFrame...")
-
     missing_values = df_cleaned.isnull().sum()
-    missing_values = missing_values[missing_values > 0]
-    missing_values_dict = missing_values.to_dict()
+    total_rows = len(df_cleaned)
     
-    if missing_values_dict:
+    missing_values_info = {
+        col: {'count': count, 'percentage': (count / total_rows) * 100}
+        for col, count in missing_values.items() if count > 0
+    }
+    
+    if missing_values_info:
         logger.info(" [⛔️] Missing values found in the following columns:")
-        for col, count in missing_values_dict.items():
-            logger.info(f" [ℹ️ ] --- > {col}: {count} missing values")
+        for col, info in missing_values_info.items():
+            logger.info(f" [ℹ️ ] --- > {col}: {info['count']} missing values ({info['percentage']:.2f}%)")
     else:
         logger.info(" [✅] No missing values found in the DataFrame.")
     
-    return missing_values_dict
+    return missing_values_info
 
 # ////////////////////////////////////////////////////////////////////////////////////
 
-def save_to_csv(df, output_dir="data/processed", filename_prefix="cleaned_data"):
+def standardize_column_names(df):
+    """
+    Standardizes column names to snake_case.
+    """
+    logger.info(" [..] Standardizing column names...")
+    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+    logger.info(" [✅] Column names standardized.")
+    return df
+
+# ////////////////////////////////////////////////////////////////////////////////////
+
+def save_to_csv(df, output_dir="data/processed", filename_prefix="cleaned_data", metadata=""):
     """
     Saves the cleaned DataFrame to a CSV file in the specified output directory.
     """
     os.makedirs(output_dir, exist_ok=True)
-    
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = f"{filename_prefix}_{timestamp}.csv"
+    filename = f"{filename_prefix}_{metadata}_{timestamp}.csv" if metadata else f"{filename_prefix}_{timestamp}.csv"
     file_path = os.path.join(output_dir, filename)
     
     try:
         df.to_csv(file_path, index=False)
         logger.info(f" [✅] Data saved successfully to {file_path}")
     except Exception as e:
-        logger.error(f" [⛔️] Failed to save data to {file_path}", exc_info=e)
+        logger.error(f" [⛔️] Failed to save data to {file_path}", exc_info=True)
     
     return file_path
+
+# ////////////////////////////////////////////////////////////////////////////////////
+
+# def open_cleaned_file(filename, output_dir="data/processed"):
+#     """
+#     Reads the cleaned CSV file from the processed directory into a DataFrame.
+#     """
+#     filepath = os.path.join(output_dir, filename)
+#     try:
+#         df = pd.read_csv(filepath)
+#         logger.info(f" [✅] Cleaned Data loaded from {filepath}")
+#         return df
+#     except Exception as e:
+#         logger.error(f" [⛔️] Error loading cleaned data from {filepath}", exc_info=True)
+#         return None
--- src/main.py
+++ src/main.py
@@ -1,23 +1,63 @@
 import logging
-from app import open_files, check_missing_values, remove_duplicates, save_to_csv
+import glob
+import os
+from app import (open_files, 
+                 check_missing_values, 
+                 remove_duplicates, 
+                 save_to_csv, 
+                 standardize_column_names,
+                 open_cleaned_file)
 
 logger = logging.getLogger(__name__)
+
+filepath = "/Users/lorenzofilippini/Desktop/datareviewer-v1/data/processed/cleaned_data_20241115_152641.csv"
+
+def log_dataframe_info(df, message=""):
+    """
+    Logs information about the DataFrame.
+    """
+    logger.info(f"{message} Shape: {df.shape}")
 
 def main():
     # Load data
     df = open_files()
-    if df is None:
+    if df.empty:
         logger.error(" [⛔️] No data loaded. Exiting program...")
         return
+    log_dataframe_info(df, "Loaded data")
     
+    # Standardize column names
+    df = standardize_column_names(df)
+    log_dataframe_info(df, "After standardizing column names")
+
     # Remove duplicates
     df_cleaned = remove_duplicates(df)
-    
+    log_dataframe_info(df_cleaned, "After removing duplicates")
+
     # Check for missing values
     missing_values = check_missing_values(df_cleaned)
-    if not missing_values:
-        logger.info(" [✅] Data Quality Check Passed: No missing values found.")
-        save_to_csv(df_cleaned)
+    if missing_values:
+        logger.warning(" [ℹ️] Missing values detected. Halting execution.")
+        return
+    
+    # Save cleaned data
+    save_to_csv(df_cleaned)
+    logger.info(" [✅] Data Cleaning Process Completed Successfully.")
+
+    # Load cleaned data
+    list_of_files = glob.glob('/Users/lorenzofilippini/Desktop/datareviewer-v1/data/processed/*.csv')
+    if not list_of_files:
+        logger.error(" [⛔️] No files found in the processed directory.")
+        return
+        
+    filepath = max(list_of_files, key=os.path.getctime)
+    cleaned_df = open_cleaned_file(filepath)
+    if cleaned_df is None:
+        logger.error(" [⛔️] Cleaned DataFrame could not be loaded from file.")
+        return
+    log_dataframe_info(cleaned_df, "After loading cleaned data")
+    
+    # Run Main Analysis
     
 
 if __name__ == '__main__':
